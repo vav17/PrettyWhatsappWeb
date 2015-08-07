f7fa0b3c3cc0281a7d0e98e8db1fd6953da174b3
vav17 <vincent.voet@hotmail.com>
2015-08-07 21:26:41 +0200

    changed image url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 setTimeout(function(){prettify();}, 1000);
 function prettify(){
-  var backgroundUrl = "http://gl0rix.net/wp-content/uploads/2015/08/gl0rix-top-background.jpg";
+  var backgroundUrl = "https://raw.githubusercontent.com/vav17/PrettyWhatsappWeb/master/img/vav17-pretty-whatsapp-background-github.jpg";
   $(".app-wrapper").css('background-image', 'url("' + backgroundUrl + '")');
   $('head').append('<style>.app-wrapper:before{height: 0px !important;}</style>');
   $(".pane-chat").css('background-image', 'none');
